2b06f8a877bcfb7d105fba9657c6ab43312908ee
Célian <celian@qs.team>
2024-11-13 01:43:58 +0100

    ✨ - Ajout d'endpoints /api/posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\BaseResource;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Resources\PostCollection;
@@ -10,11 +11,35 @@ use App\Http\Resources\PostResource;
 class PostController extends Controller
 {
     public function index($id = null) {
-        if ($id) {
-            $video = Post::findOrFail($id);
-            return new PostResource($video);
-        }
-        $video = Post::where("post_type", "post")->get();
-        return new PostCollection($video);
+        $posts = Post::where("post_type", "post")->get();
+        $collection = new PostCollection($posts);
+
+        return (new BaseResource($collection))
+            ->success()
+            ->setCode(200)
+            ->setMessage("Post list loaded successfully");
     }
+    public function show($id) {
+        $post = Post::findOrFail($id);
+        $resource = new PostResource($post);
+
+        return $resource
+            ->success()
+            ->setCode(200)
+            ->setMessage("Post retrieved successfully");
+    }
+
+    public function store(Request $request) {
+        var_dump($request->all());die;
+        // TODO : Continue request
+        $post = Post::create($request->all());
+        $resource = new PostResource($post);
+
+        return $resource
+            ->success()
+            ->setCode(201)
+            ->setMessage("Post created successfully");
+    }
+
+
 }
